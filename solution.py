298c510275d105372c1406cb7cba1bda4a6e2b41
pattonw <pattonw@hhmi.org>
2023-06-08 13:27:54 -0700

    Add Exercises for train/validate blocks

--- solution.py
+++ solution.py
@@ -624,7 +624,84 @@ assert dice(wrong_prediction, target) == 0.0
 # Let's start with writing training and validation functions.
 
 # %% [markdown]
-# __Task 5.1__: fix in all the TODOs to make the train function work. If confused, you can use this [PyTorch tutorial](https://pytorch.org/tutorials/beginner/basics/optimization_tutorial.html) as a template
+# <div class="alert alert-block alert-info">
+#     <b>Task 5.2</b>: fix in all the TODOs to make the train function work. If confused, you can use this [PyTorch tutorial](https://pytorch.org/tutorials/beginner/basics/optimization_tutorial.html) as a template
+# </div>
+
+
+# %%
+# apply training for one epoch
+def train(
+    model,
+    loader,
+    optimizer,
+    loss_function,
+    epoch,
+    log_interval=100,
+    log_image_interval=20,
+    tb_logger=None,
+    device=None,
+    early_stop=False,
+):
+    if device is None:
+        # You can pass in a device or we will default to using
+        # the gpu. Feel free to try training on the cpu to see
+        # what sort of performance difference there is
+        if torch.cuda.is_available():
+            device = torch.device("cuda")
+        else:
+            device = torch.device("cpu")
+
+    # set the model to train mode
+    model.train()
+
+    # move model to device
+    model = model.to(device)
+
+    # iterate over the batches of this epoch
+    for batch_id, (x, y) in enumerate(loader):
+        # move input and target to the active device (either cpu or gpu)
+        x, y = x.to(device), y.to(device)
+
+        # apply model and calculate loss
+        prediction = ...  # placeholder since we use prediction later
+        loss = ...  # placeholder since we use the loss later
+
+        # log to console
+        if batch_id % log_interval == 0:
+            print(
+                "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+                    epoch,
+                    batch_id * len(x),
+                    len(loader.dataset),
+                    100.0 * batch_id / len(loader),
+                    loss.item(),
+                )
+            )
+
+        # log to tensorboard
+        if tb_logger is not None:
+            step = epoch * len(loader) + batch_id
+            tb_logger.add_scalar(
+                tag="train_loss", scalar_value=loss.item(), global_step=step
+            )
+            # check if we log images in this iteration
+            if step % log_image_interval == 0:
+                tb_logger.add_images(
+                    tag="input", img_tensor=x.to("cpu"), global_step=step
+                )
+                tb_logger.add_images(
+                    tag="target", img_tensor=y.to("cpu"), global_step=step
+                )
+                tb_logger.add_images(
+                    tag="prediction",
+                    img_tensor=prediction.to("cpu").detach(),
+                    global_step=step,
+                )
+
+        if early_stop and batch_id > 5:
+            print("Stopping test early!")
+            break
 
 
 # %% tags=["solution"]
@@ -709,6 +786,7 @@ def train(
             break
 
 
+# %%
 # Quick sanity check for your train function to make sure no errors are thrown:
 # Good place to test unetA, unetB, unetC, unetD to see if you can eliminate some
 simple_net = UNet(1, 1, depth=1, final_activation=nn.Sigmoid())
@@ -728,6 +806,70 @@ train(
 # <div class="alert alert-block alert-info">
 #     <b>Task 5.2</b>: fix in all the TODOs to make the validate function work. If confused, you can use this <a href="https://pytorch.org/tutorials/beginner/basics/optimization_tutorial.html">PyTorch tutorial</a> as a template
 # </div>
+
+
+# %%
+# run validation after training epoch
+def validate(
+    model,
+    loader,
+    loss_function,
+    metric,
+    step=None,
+    tb_logger=None,
+    device=None,
+):
+    if device is None:
+        # You can pass in a device or we will default to using
+        # the gpu. Feel free to try training on the cpu to see
+        # what sort of performance difference there is
+        if torch.cuda.is_available():
+            device = torch.device("cuda")
+        else:
+            device = torch.device("cpu")
+
+    # set model to eval mode
+    model.eval()
+    model.to(device)
+
+    # running loss and metric values
+    val_loss = 0
+    val_metric = 0
+
+    # disable gradients during validation
+    with torch.no_grad():
+        # iterate over validation loader and update loss and metric values
+        for x, y in loader:
+            x, y = x.to(device), y.to(device)
+            # TODO: evaluate this example with the given loss and metric
+            prediction = ...
+            val_loss += ...
+            val_metric += ...
+
+    # normalize loss and metric
+    val_loss /= len(loader)
+    val_metric /= len(loader)
+
+    if tb_logger is not None:
+        assert (
+            step is not None
+        ), "Need to know the current step to log validation results"
+        tb_logger.add_scalar(tag="val_loss", scalar_value=val_loss, global_step=step)
+        tb_logger.add_scalar(
+            tag="val_metric", scalar_value=val_metric, global_step=step
+        )
+        # we always log the last validation images
+        tb_logger.add_images(tag="val_input", img_tensor=x.to("cpu"), global_step=step)
+        tb_logger.add_images(tag="val_target", img_tensor=y.to("cpu"), global_step=step)
+        tb_logger.add_images(
+            tag="val_prediction", img_tensor=prediction.to("cpu"), global_step=step
+        )
+
+    print(
+        "\nValidate: Average loss: {:.4f}, Average Metric: {:.4f}\n".format(
+            val_loss, val_metric
+        )
+    )
 
 
 # %% tags=["solution"]
