93a7d6fe194fae87aa2758ad9941bbdddc51b5d0
pattonw <pattonw@hhmi.org>
2023-06-08 13:41:04 -0700

    replace UNetGN solution with a subclass of UNet
    teach good code reuse

--- solution.py
+++ solution.py
@@ -1025,7 +1025,7 @@ for epoch in range(n_epochs):
 
 
 # %%
-class UNetGN(nn.Module):
+class UNetGN(UNet):
     """UNet implementation with Group Norm
     Arguments:
       in_channels: number of input channels
@@ -1039,112 +1039,14 @@ class UNetGN(nn.Module):
     # the model implementation with different architecture elements
 
     # Convolutional block for single layer of the decoder / encoder
-    # we apply two 2d convolutions with relu activation and group norm
+    # we apply two 2d convolutions with relu activation
     def _conv_block(self, in_channels, out_channels):
         return nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.GroupNorm(8, out_channels),
             nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.GroupNorm(8, out_channels),
         )
-
-    # upsampling via transposed 2d convolutions
-    def _upsampler(self, in_channels, out_channels):
-        return nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
-
-    def __init__(self, in_channels=1, out_channels=1, depth=4, final_activation=None):
-        super().__init__()
-
-        assert depth < 7, "Max supported depth is 7"
-
-        # the depth (= number of encoder / decoder levels) is
-        # hard-coded to 4
-        self.depth = depth
-
-        # the final activation must either be None or a Module
-        if final_activation is not None:
-            assert isinstance(
-                final_activation, nn.Module
-            ), "Activation must be torch module"
-
-        # all lists of conv layers (or other nn.Modules with parameters) must be wraped
-        # itnto a nn.ModuleList
-
-        # modules of the encoder path
-        self.encoder = nn.ModuleList(
-            [
-                self._conv_block(in_channels, 16),
-                self._conv_block(16, 32),
-                self._conv_block(32, 64),
-                self._conv_block(64, 128),
-                self._conv_block(128, 256),
-                self._conv_block(256, 512),
-                self._conv_block(512, 1024),
-            ][:depth]
-        )
-        # the base convolution block
-        if depth >= 1:
-            self.base = self._conv_block(2 ** (depth + 3), 2 ** (depth + 4))
-        else:
-            self.base = self._conv_block(1, 2 ** (depth + 4))
-        # modules of the decoder path
-        self.decoder = nn.ModuleList(
-            [
-                self._conv_block(2048, 1024),
-                self._conv_block(1024, 512),
-                self._conv_block(512, 256),
-                self._conv_block(256, 128),
-                self._conv_block(128, 64),
-                self._conv_block(64, 32),
-                self._conv_block(32, 16),
-            ][-depth:]
-        )
-
-        # the pooling layers; we use 2x2 MaxPooling
-        self.poolers = nn.ModuleList([nn.MaxPool2d(2) for _ in range(self.depth)])
-        # the upsampling layers
-        self.upsamplers = nn.ModuleList(
-            [
-                self._upsampler(2048, 1024),
-                self._upsampler(1024, 512),
-                self._upsampler(512, 256),
-                self._upsampler(256, 128),
-                self._upsampler(128, 64),
-                self._upsampler(64, 32),
-                self._upsampler(32, 16),
-            ][-depth:]
-        )
-        # output conv and activation
-        # the output conv is not followed by a non-linearity, because we apply
-        # activation afterwards
-        self.out_conv = nn.Conv2d(16, out_channels, 1)
-        self.activation = final_activation
-
-    def forward(self, input):
-        x = input
-        # apply encoder path
-        encoder_out = []
-        for level in range(self.depth):
-            x = self.encoder[level](x)
-            encoder_out.append(x)
-            x = self.poolers[level](x)
-
-        # apply base
-        x = self.base(x)
-
-        # apply decoder path
-        encoder_out = encoder_out[::-1]
-        for level in range(self.depth):
-            x = self.upsamplers[level](x)
-            x = self.decoder[level](torch.cat((x, encoder_out[level]), dim=1))
-
-        # apply output conv and activation (if given)
-        x = self.out_conv(x)
-        if self.activation is not None:
-            x = self.activation(x)
-        return x
 
 
 # %%
