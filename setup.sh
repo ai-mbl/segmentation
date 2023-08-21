53cdc9ee404e1291be2b04775b511f0f2a1684cd
Rachel Ware <riware@uw.edu>
2023-08-21 17:33:59 -0400

    update setup.sh to properly activate our environment

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+# this seems necessary for the activate call to work
+eval "$(conda shell.bash hook)"
 # Create environment name based on the exercise name
 mamba create -n 05-image-segmentation python=3.11 -y
 mamba activate 05-image-segmentation
